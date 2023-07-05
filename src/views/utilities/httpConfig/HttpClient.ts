4a963678f9feb1cea0851ff0674f7463e4696e14
明 何 <hm80@vip.qq.com>
2023-07-05 17:57:44 +0800

    ts编写

--- src/views/utilities/httpConfig/HttpClient.ts
+++ src/views/utilities/httpConfig/HttpClient.ts
@@ -3,32 +3,32 @@ import axios, {AxiosInstance} from 'axios';
 import {Message} from "../Message/Message";
 
 // 声明 Promise<any> 类型
-
+const  msg = new Message();
 class HttpClient {
-    private Instance: AxiosInstance;
-    private Message: Message;
+     Instance: AxiosInstance;
     constructor() {
         this.Instance = axios.create({
             baseURL: 'http://localhost:8080',
             timeout: 1000,
             headers: {'X-Custom-Header': 'foobar'}
         });
+
         }
-    public async Get(url: string, params: any): Promise<any> {
+    public static async Get(url: string, params: any){
         this.Instance.get(url, params).then((response) => {
             return response;
 
         }).catch((error) => {
-           this.Message.Error('错误',error);
+            msg.Error('错误',error);
         });
     }
 
-    public async Post(url: string, params: any): Promise<any> {
+    public static async Post(url: string, params: any) {
         this.Instance.post(url, params).then((response) => {
             return response;
 
         }).catch((error) => {
-            this.Message.Error('错误',error);
+            msg.Error('错误',error);
         });
     }
 }
